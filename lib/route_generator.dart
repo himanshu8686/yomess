fd9a876342f449fb7f65a95f070356a1bd4813db
Milan R Dhameliya <dhameliyamilan972@gmail.com>
2020-11-27 21:56:48 +0530

    Login Signup Forgot Password Design Done - Milan R Dhameliya

---
+++ lib/route_generator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:indglobalyomess/screens/ForgotPasswordScreen.dart';
+import 'package:indglobalyomess/screens/LoginScreen.dart';
+import 'package:indglobalyomess/screens/SingupScreen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting arguments passed in while calling Navigator.pushNamed
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/Login':
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case '/SignUp':
+        return MaterialPageRoute(builder: (_) => SignupScreen());
+      case '/ForgetPassword':
+        return MaterialPageRoute(builder: (_) => ForgotPasswordScreen());
+      default:
+        // If there is no such named route in the switch statement, e.g. /third
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: SafeArea(
+              child: Text('Route Error'),
+            ),
+          ),
+        );
+    }
+  }
+}
